51254358795619a84d99221b2c66f78357289451
sunxiaohang <sxh13208803520@gmail.com>
2020-06-07 21:33:56 +0800

    init

---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    public float m_Velocity = 1f;
+
+    public bool m_Forward = true;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(m_Velocity * Time.deltaTime,0,0);
+        if (transform.position.x >= 5)
+        {
+            m_Forward = false;
+        }
+
+        if (transform.position.x <= -5)
+        {
+            m_Forward = true;
+        }
+
+        if (m_Forward)
+        {
+            transform.position = transform.position + movement;
+        }
+        if(!m_Forward)
+        {
+            transform.position = transform.position - movement;
+        }
+    }
+}
---
+++ Assets/Scripts/SpiderLegController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SpiderLegController : MonoBehaviour
+{
+	public Transform m_TargetTransform;
+	private Vector3 m_PreviewTargetPosition;
+	public Transform m_FixedRayCastTransform;
+	public Vector3 desirePosition;
+	public LayerMask m_FloorLayerMask;
+	public float m_StepDistance = 0.5f;
+	public AnimationCurve m_TargetUpwardCure;
+	public AnimationCurve m_TargetTowardCure;
+	private float m_StepDuration = .3f;
+	private float m_LastStepTimeStamp;
+	private bool m_Moving = false;
+	private void Start()
+	{
+		m_PreviewTargetPosition = m_TargetTransform.position;
+		m_LastStepTimeStamp = Time.time;
+	}
+	private void Update()
+	{
+		if(!m_Moving)m_TargetTransform.position = m_PreviewTargetPosition;
+		UpdateIkTarget();
+		RaycastHit hitInfo;
+		if (Physics.Raycast(m_FixedRayCastTransform.position,Vector3.down, out hitInfo, 2, m_FloorLayerMask))
+		{
+			desirePosition = hitInfo.point;
+			if (Vector3.Distance(m_FixedRayCastTransform.position, m_TargetTransform.position) > m_StepDistance)
+			{
+				Vector3 position = desirePosition+new Vector3(0,0.5f,0);
+				m_PreviewTargetPosition = position;
+				m_LastStepTimeStamp = Time.time;
+				m_Moving = true;
+			}
+		}
+	}
+
+	void UpdateIkTarget()
+	{
+		if (!m_Moving) return;
+		float percent = Mathf.Clamp01((Time.time-m_LastStepTimeStamp)/m_StepDuration);
+		Vector3 targetTransformPosition = m_TargetTransform.position;
+		float offsetY = targetTransformPosition.y-m_PreviewTargetPosition.y;
+		Vector3 directionYCurve = m_TargetUpwardCure.Evaluate(percent)*0.25f*Vector3.up-new Vector3(0,offsetY,0);
+		Debug.Log("offset:"+directionYCurve);
+		Debug.Log("distance:"+Vector3.Distance(m_PreviewTargetPosition,targetTransformPosition));
+		m_TargetTransform.position = Vector3.Lerp(targetTransformPosition, m_PreviewTargetPosition,m_TargetTowardCure.Evaluate(percent))+directionYCurve;
+		if (Mathf.Approximately(1,percent)) m_Moving = false;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(m_TargetTransform.position,m_FixedRayCastTransform.position);
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere(desirePosition,0.03f);
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(m_FixedRayCastTransform.position,desirePosition);
+	}
+}
